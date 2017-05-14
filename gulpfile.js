e66ef68bf5d22d416371cd2310fd81eeac1806ca
Luan Gjokaj <luan@deep-impact.ch>
2017-05-14 16:19:08 +0200

    Formatting.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 /* -------------------------------------------------------------------------------------------------
 
-    Build Configuration
-    Contributors: Luan Gjokaj
+	Build Configuration
+	Contributors: Luan Gjokaj
 
-------------------------------------------------------------------------------------------------- */
+ ------------------------------------------------------------------------------------------------- */
 'use strict';
 var gulp = require('gulp');
 var gutil = require('gulp-util');
@@ -23,215 +23,215 @@ var fileinclude = require('gulp-file-include');
 var modRewrite = require('connect-modrewrite');
 //--------------------------------------------------------------------------------------------------
 /* -------------------------------------------------------------------------------------------------
-    PostCSS Plugins
-------------------------------------------------------------------------------------------------- */
+	PostCSS Plugins
+ ------------------------------------------------------------------------------------------------- */
 var pluginsDev = [
-    partialimport,
-    cssnext({
-        warnForDuplicates: false
-    })
+	partialimport,
+	cssnext({
+		warnForDuplicates: false
+	})
 ];
 var pluginsProd = [
-    partialimport,
-    cssnext({
-        warnForDuplicates: false
-    }),
-    cssnano()
+	partialimport,
+	cssnext({
+		warnForDuplicates: false
+	}),
+	cssnano()
 ];
 //--------------------------------------------------------------------------------------------------
 /* -------------------------------------------------------------------------------------------------
-    Your JavaScript Files
-------------------------------------------------------------------------------------------------- */
+	Your JavaScript Files
+ ------------------------------------------------------------------------------------------------- */
 var headerJS = [
-    'node_modules/aos/dist/aos.js'
+	'node_modules/aos/dist/aos.js'
 ];
 var footerJS = [
-    'node_modules/jquery/dist/jquery.js',
-    'src/assets/js/**'
+	'node_modules/jquery/dist/jquery.js',
+	'src/assets/js/**'
 ];
 //--------------------------------------------------------------------------------------------------
 /* -------------------------------------------------------------------------------------------------
-    Start of Build Tasks
-------------------------------------------------------------------------------------------------- */
+	Start of Build Tasks
+ ------------------------------------------------------------------------------------------------- */
 gulp.task('build-dev', [
-    'style-dev',
-    'copy-images',
-    'copy-fonts',
-    'header-scripts-dev',
-    'footer-scripts-dev',
-    'process-static-files-dev',
-    'watch'
+	'style-dev',
+	'copy-images',
+	'copy-fonts',
+	'header-scripts-dev',
+	'footer-scripts-dev',
+	'process-static-files-dev',
+	'watch'
 ], function () {
-    browserSync.init({
-        server: {
-            baseDir: "app"
-        },
-        middleware: [
-            modRewrite([
-                '^.([^\\.]+)$ /$1.html [L]'
-            ])
-        ]
-    });
+	browserSync.init({
+		server: {
+			baseDir: "app"
+		},
+		middleware: [
+			modRewrite([
+				'^.([^\\.]+)$ /$1.html [L]'
+			])
+		]
+	});
 });
 
 gulp.task('build-prod', [
-    'style-prod',
-    'copy-images',
-    'copy-fonts',
-    'header-scripts-prod',
-    'footer-scripts-prod',
-    'process-static-files-prod'
+	'style-prod',
+	'copy-images',
+	'copy-fonts',
+	'header-scripts-prod',
+	'footer-scripts-prod',
+	'process-static-files-prod'
 ]);
 
 gulp.task('default');
 
 gulp.task('process-static-files-dev', function () {
-    return gulp.src(['src/*.html'])
-        .pipe(plumber({ errorHandler: onError }))
-        .pipe(fileinclude({
-            filters: {
-                prefix: '@@',
-                basepath: '@file'
-            }
-        }))
-        .pipe(gulp.dest('app'));
+	return gulp.src(['src/*.html'])
+		.pipe(plumber({ errorHandler: onError }))
+		.pipe(fileinclude({
+			filters: {
+				prefix: '@@',
+				basepath: '@file'
+			}
+		}))
+		.pipe(gulp.dest('app'));
 });
 
 gulp.task('process-static-files-prod', function () {
-    return gulp.src(['src/*.html'])
-        .pipe(plumber({ errorHandler: onError }))
-        .pipe(fileinclude({
-            filters: {
-                prefix: '@@',
-                basepath: '@file'
-            }
-        }))
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            ignoreCustomFragments: [/<%[\s\S]*?%>/, /<\?[=|php]?[\s\S]*?\?>/]
-        }))
-        .pipe(gulp.dest('app'));
+	return gulp.src(['src/*.html'])
+		.pipe(plumber({ errorHandler: onError }))
+		.pipe(fileinclude({
+			filters: {
+				prefix: '@@',
+				basepath: '@file'
+			}
+		}))
+		.pipe(htmlmin({
+			collapseWhitespace: true,
+			ignoreCustomFragments: [/<%[\s\S]*?%>/, /<\?[=|php]?[\s\S]*?\?>/]
+		}))
+		.pipe(gulp.dest('app'));
 });
 
 gulp.task('footer-scripts-dev', function () {
-    return gulp.src(footerJS)
-        .pipe(plumber({ errorHandler: onError }))
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(concat('bundle.js'))
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest('app/assets/js'));
+	return gulp.src(footerJS)
+		.pipe(plumber({ errorHandler: onError }))
+		.pipe(sourcemaps.init())
+		.pipe(babel({
+			presets: ['env']
+		}))
+		.pipe(concat('bundle.js'))
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest('app/assets/js'));
 });
 
 /* NOTE: On header scripts babel is not executed */
 gulp.task('header-scripts-dev', function () {
-    return gulp.src(headerJS)
-        .pipe(plumber({ errorHandler: onError }))
-        .pipe(sourcemaps.init())
-        .pipe(concat('top.js'))
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest('app/assets/js'));
+	return gulp.src(headerJS)
+		.pipe(plumber({ errorHandler: onError }))
+		.pipe(sourcemaps.init())
+		.pipe(concat('top.js'))
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest('app/assets/js'));
 });
 
 gulp.task('footer-scripts-prod', function () {
-    return gulp.src(footerJS)
-        .pipe(plumber({ errorHandler: onError }))
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(concat('bundle.js'))
-        .pipe(uglify())
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest('app/assets/js'));
+	return gulp.src(footerJS)
+		.pipe(plumber({ errorHandler: onError }))
+		.pipe(babel({
+			presets: ['env']
+		}))
+		.pipe(concat('bundle.js'))
+		.pipe(uglify())
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest('app/assets/js'));
 });
 
 /* NOTE: On header scripts babel is not executed */
 gulp.task('header-scripts-prod', function () {
-    return gulp.src(headerJS)
-        .pipe(plumber({ errorHandler: onError }))
-        .pipe(concat('top.js'))
-        .pipe(uglify())
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest('app/assets/js'));
+	return gulp.src(headerJS)
+		.pipe(plumber({ errorHandler: onError }))
+		.pipe(concat('top.js'))
+		.pipe(uglify())
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest('app/assets/js'));
 });
 
 gulp.task('copy-fonts', function () {
-    return gulp.src('src/assets/fonts/**')
-        .pipe(plumber({ errorHandler: onError }))
-        .pipe(gulp.dest('app/assets/fonts'));
+	return gulp.src('src/assets/fonts/**')
+		.pipe(plumber({ errorHandler: onError }))
+		.pipe(gulp.dest('app/assets/fonts'));
 });
 
 gulp.task('copy-images', function () {
-    return gulp.src('src/assets/img/**')
-        .pipe(plumber({ errorHandler: onError }))
-        .pipe(gulp.dest('app/assets/img'));
+	return gulp.src('src/assets/img/**')
+		.pipe(plumber({ errorHandler: onError }))
+		.pipe(gulp.dest('app/assets/img'));
 });
 
 gulp.task('process-images', function () {
-    return gulp.src('src/assets/img/**')
-        .pipe(plumber({ errorHandler: onError }))
-        .pipe(imagemin([
-            imagemin.svgo({plugins: [{removeViewBox: true}]})
-        ], {
-            verbose: true
-        }))
-        .pipe(gulp.dest('app/assets/img'));
+	return gulp.src('src/assets/img/**')
+		.pipe(plumber({ errorHandler: onError }))
+		.pipe(imagemin([
+			imagemin.svgo({ plugins: [{ removeViewBox: true }] })
+		], {
+			verbose: true
+		}))
+		.pipe(gulp.dest('app/assets/img'));
 });
 
 gulp.task('style-dev', function () {
-    return gulp.src('src/assets/style/main.css')
-        .pipe(plumber({ errorHandler: onError }))
-        .pipe(sourcemaps.init())
-        .pipe(postcss(pluginsDev))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('app/assets/css'))
-        .pipe(browserSync.stream({match: '**/*.css'}));
+	return gulp.src('src/assets/style/main.css')
+		.pipe(plumber({ errorHandler: onError }))
+		.pipe(sourcemaps.init())
+		.pipe(postcss(pluginsDev))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('app/assets/css'))
+		.pipe(browserSync.stream({ match: '**/*.css' }));
 });
 
 gulp.task('style-prod', function () {
-    return gulp.src('src/assets/style/main.css')
-        .pipe(plumber({ errorHandler: onError }))
-        .pipe(postcss(pluginsProd))
-        .pipe(gulp.dest('app/assets/css'));
+	return gulp.src('src/assets/style/main.css')
+		.pipe(plumber({ errorHandler: onError }))
+		.pipe(postcss(pluginsProd))
+		.pipe(gulp.dest('app/assets/css'));
 });
 
 var onError = function (err) {
-    gutil.beep();
-    console.log(err.toString());
-    this.emit('end');
+	gutil.beep();
+	console.log(err.toString());
+	this.emit('end');
 };
 
 var reload = browserSync.reload();
 
 gulp.task('reload-js', ['footer-scripts-dev'], function (done) {
-    browserSync.reload();
-    done();
+	browserSync.reload();
+	done();
 });
 
 gulp.task('reload-images', ['copy-images'], function (done) {
-    browserSync.reload();
-    done();
+	browserSync.reload();
+	done();
 });
 
 gulp.task('reload-fonts', ['copy-fonts'], function (done) {
-    browserSync.reload();
-    done();
+	browserSync.reload();
+	done();
 });
 
 gulp.task('reload-files', ['process-static-files-dev'], function (done) {
-    browserSync.reload();
-    done();
+	browserSync.reload();
+	done();
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['src/assets/style/**/*.css'], ['style-dev']);
-    gulp.watch(['src/assets/js/**'], ['reload-js']);
-    gulp.watch(['src/assets/img/**'], ['reload-images']);
-    gulp.watch(['src/assets/fonts/**'], ['reload-fonts']);
-    gulp.watch(['src/*.html', 'src/includes/**'], ['reload-files']);
+	gulp.watch(['src/assets/style/**/*.css'], ['style-dev']);
+	gulp.watch(['src/assets/js/**'], ['reload-js']);
+	gulp.watch(['src/assets/img/**'], ['reload-images']);
+	gulp.watch(['src/assets/fonts/**'], ['reload-fonts']);
+	gulp.watch(['src/*.html', 'src/includes/**'], ['reload-files']);
 });
 /* -------------------------------------------------------------------------------------------------
-    End of Build Tasks
-------------------------------------------------------------------------------------------------- */
+	End of Build Tasks
+ ------------------------------------------------------------------------------------------------- */
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,9 @@
 const test = 'Babel is doing the job.';
 
-$(window).on('load', function() {
-    console.log(test);
-
-    setTimeout(function () {
-        $('.last').addClass('last-loaded');
-    }, 2000);
-    AOS.init();
+$(window).on('load', function () {
+	console.log(test);
+	setTimeout(function () {
+		$('.last').addClass('last-loaded');
+	}, 2000);
+	AOS.init();
 });
