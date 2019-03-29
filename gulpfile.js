26f493502a08ad76c02cb82c4c85d32239d73b12
Luan Gjokaj <luan@riangle.com>
2019-03-29 15:17:49 +0100

    🚀 RELEASE: Update to Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,25 @@ Build Configuration
 Contributors: Luan Gjokaj
 
 -------------------------------------------------------------------------------------------------- */
+const { gulp, series, parallel, dest, src, watch } = require('gulp');
 const babel = require('gulp-babel');
 const browserSync = require('browser-sync').create();
 const cachebust = require('gulp-cache-bust');
 const concat = require('gulp-concat');
 const cssnano = require('cssnano');
-const postcssPresetEnv = require('postcss-preset-env');
 const del = require('del');
 const fileinclude = require('gulp-file-include');
-const gulp = require('gulp');
 const gutil = require('gulp-util');
 const htmlmin = require('gulp-htmlmin');
 const imagemin = require('gulp-imagemin');
 const modRewrite = require('connect-modrewrite');
-const postcssImport = require('postcss-import');
 const plumber = require('gulp-plumber');
 const postcss = require('gulp-postcss');
+const postcssImport = require('postcss-import');
+const postcssPresetEnv = require('postcss-preset-env');
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
+
 //--------------------------------------------------------------------------------------------------
 /* -------------------------------------------------------------------------------------------------
 PostCSS Plugins
@@ -47,83 +48,101 @@ const pluginsProd = [
 			'custom-media': true,
 		},
 	}),
-	cssnano({
-		reduceIdents: false,
+	require('cssnano')({
+		preset: [
+			'default',
+			{
+				discardComments: true,
+			},
+		],
 	}),
 ];
+
 //--------------------------------------------------------------------------------------------------
 /* -------------------------------------------------------------------------------------------------
 Header & Footer JavaScript Boundles
 -------------------------------------------------------------------------------------------------- */
-const headerJS = ['src/etc/analytics.js', 'node_modules/aos/dist/aos.js'];
-const footerJS = ['node_modules/jquery/dist/jquery.js', 'src/assets/js/**'];
+const headerJS = [
+	'./src/etc/analytics.js',
+	'./node_modules/aos/dist/aos.js'
+];
+const footerJS = [
+	'./node_modules/jquery/dist/jquery.js',
+	'./src/assets/js/**'
+];
+
 //--------------------------------------------------------------------------------------------------
 /* -------------------------------------------------------------------------------------------------
 Development Tasks
 -------------------------------------------------------------------------------------------------- */
-gulp.task(
-	'build-dev',
-	[
-		'cleanup',
-		'style-dev',
-		'copy-images',
-		'copy-fonts',
-		'header-scripts-dev',
-		'footer-scripts-dev',
-		'process-static-files-dev',
-		'watch',
-	],
-	() => {
-		browserSync.init({
+function devServer() {
+	browserSync.init({
+			logPrefix: 'Static Web Build',
 			server: {
-				baseDir: 'app',
-			},
-			middleware: [modRewrite(['^.([^\\.]+)$ /$1.html [L]'])],
-		});
-	},
-);
+			baseDir: './build',
+		},
+		middleware: [modRewrite(['^.([^\\.]+)$ /$1.html [L]'])],
+	});
 
-gulp.task('default');
+	watch('./src/assets/css/**/*.css', stylesDev);
+	watch('./src/assets/js/**', series(footerScriptsDev, Reload));
+	watch('./src/assets/img/**', series(copyImagesDev, Reload));
+	watch('./src/assets/fonts/**', series(copyFontsDev, Reload));
+	watch('./src/*.html', series(staticFilesDev, Reload));
+};
 
-gulp.task('style-dev', () => {
-	return gulp
-		.src('src/assets/style/main.css')
+function Reload(done) {
+	browserSync.reload();
+	done();
+}
+
+function copyImagesDev() {
+	return src('./src/assets/img/**').pipe(
+		dest('./build/assets/img'),
+	);
+}
+
+function copyFontsDev() {
+	return src('./src/assets/fonts/**').pipe(
+		dest('./build/assets/fonts'),
+	);
+}
+
+function stylesDev() {
+	return src('./src/assets/css/main.css')
 		.pipe(plumber({ errorHandler: onError }))
 		.pipe(sourcemaps.init())
 		.pipe(postcss(pluginsDev))
 		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('app/assets/css'))
+		.pipe(dest('./build/assets/css'))
 		.pipe(browserSync.stream({ match: '**/*.css' }));
-});
+}
 
-gulp.task('header-scripts-dev', () => {
-	return gulp
-		.src(headerJS)
+function headerScriptsDev() {
+	return src(headerJS)
 		.pipe(plumber({ errorHandler: onError }))
 		.pipe(sourcemaps.init())
-		.pipe(concat('top.js'))
+		.pipe(concat('header-bundle.js'))
 		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('app/assets/js'));
-});
+		.pipe(dest('./build/assets/js'));
+}
 
-gulp.task('footer-scripts-dev', () => {
-	return gulp
-		.src(footerJS)
+function footerScriptsDev() {
+	return src(footerJS)
 		.pipe(plumber({ errorHandler: onError }))
 		.pipe(sourcemaps.init())
 		.pipe(
 			babel({
-				presets: ['@babel/env'],
+				presets: ['@babel/preset-env'],
 			}),
 		)
-		.pipe(concat('bundle.js'))
+		.pipe(concat('footer-bundle.js'))
 		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('app/assets/js'));
-});
+		.pipe(dest('./build/assets/js'));
+}
 
-gulp.task('process-static-files-dev', () => {
-	return gulp
-		.src(['src/*.html'])
+function staticFilesDev() {
+	return src('./src/*.html')
 		.pipe(plumber({ errorHandler: onError }))
 		.pipe(
 			fileinclude({
@@ -138,100 +157,62 @@ gulp.task('process-static-files-dev', () => {
 				type: 'timestamp',
 			}),
 		)
-		.pipe(gulp.dest('app'));
-});
+		.pipe(dest('./build'));
+}
 
-gulp.task('reload-js', ['footer-scripts-dev'], function(done) {
-	browserSync.reload();
-	done();
-});
+exports.dev = series(
+	copyImagesDev,
+	copyFontsDev,
+	stylesDev,
+	headerScriptsDev,
+	footerScriptsDev,
+	staticFilesDev,
+	devServer,
+);
 
-gulp.task('reload-images', ['copy-images'], function(done) {
-	browserSync.reload();
-	done();
-});
-
-gulp.task('reload-fonts', ['copy-fonts'], function(done) {
-	browserSync.reload();
-	done();
-});
-
-gulp.task('reload-files', ['process-static-files-dev'], function(done) {
-	browserSync.reload();
-	done();
-});
-
-gulp.task('watch', () => {
-	gulp.watch(['src/assets/style/**/*.css'], ['style-dev']);
-	gulp.watch(['src/assets/js/**'], ['reload-js']);
-	gulp.watch(['src/assets/img/**'], ['reload-images']);
-	gulp.watch(['src/assets/fonts/**'], ['reload-fonts']);
-	gulp.watch(['src/*.html', 'src/includes/**'], ['reload-files']);
-});
-//--------------------------------------------------------------------------------------------------
 /* -------------------------------------------------------------------------------------------------
 Production Tasks
 -------------------------------------------------------------------------------------------------- */
-gulp.task(
-	'build-prod',
-	[
-		'cleanup',
-		'style-prod',
-		'copy-htaccess',
-		'copy-images',
-		'copy-fonts',
-		'header-scripts-prod',
-		'footer-scripts-prod',
-		'process-static-files-prod',
-	],
-	() => {
-		gutil.beep();
-		gutil.log(filesGenerated);
-		gutil.log(thankYou);
-	},
-);
+async function cleanProd() {
+	await del(['./dist']);
+}
 
-gulp.task('style-prod', () => {
-	return gulp
-		.src('src/assets/style/main.css')
+function copyFontsProd() {
+	return src('./src/assets/fonts/**').pipe(
+		dest('./dist/assets/fonts'),
+	);
+}
+
+function stylesProd() {
+	return src('./src/assets/css/main.css')
 		.pipe(plumber({ errorHandler: onError }))
 		.pipe(postcss(pluginsProd))
-		.pipe(gulp.dest('app/assets/css'));
-});
+		.pipe(dest('./dist/assets/css'));
+}
 
-gulp.task('copy-htaccess', () => {
-	return gulp
-		.src('src/etc/.htaccess')
+function headerScriptsProd() {
+	return src(headerJS)
 		.pipe(plumber({ errorHandler: onError }))
-		.pipe(gulp.dest('app'));
-});
-
-gulp.task('header-scripts-prod', () => {
-	return gulp
-		.src(headerJS)
-		.pipe(plumber({ errorHandler: onError }))
-		.pipe(concat('top.js'))
+		.pipe(concat('header-bundle.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('app/assets/js'));
-});
+		.pipe(dest('./dist/assets/js'));
+}
 
-gulp.task('footer-scripts-prod', () => {
-	return gulp
-		.src(footerJS)
+function footerScriptsProd() {
+	return src(footerJS)
 		.pipe(plumber({ errorHandler: onError }))
 		.pipe(
 			babel({
-				presets: ['@babel/env'],
+				presets: ['@babel/preset-env'],
 			}),
 		)
-		.pipe(concat('bundle.js'))
+		.pipe(concat('footer-bundle.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('app/assets/js'));
-});
+		.pipe(dest('./dist/assets/js'));
+}
 
-gulp.task('process-static-files-prod', () => {
-	return gulp
-		.src(['src/*.html'])
+function staticFilesProd() {
+	return src('./src/*.html')
 		.pipe(plumber({ errorHandler: onError }))
 		.pipe(
 			fileinclude({
@@ -252,57 +233,56 @@ gulp.task('process-static-files-prod', () => {
 				ignoreCustomFragments: [/<%[\s\S]*?%>/, /<\?[=|php]?[\s\S]*?\?>/],
 			}),
 		)
-		.pipe(gulp.dest('app'));
-});
-//--------------------------------------------------------------------------------------------------
-/* -------------------------------------------------------------------------------------------------
-Shared Tasks
--------------------------------------------------------------------------------------------------- */
-gulp.task('cleanup', () => {
-	del.sync(['app/**']);
-});
+		.pipe(dest('./dist'));
+}
 
-gulp.task('copy-images', () => {
-	return gulp
-		.src('src/assets/img/**')
-		.pipe(plumber({ errorHandler: onError }))
-		.pipe(gulp.dest('app/assets/img'));
-});
-
-gulp.task('copy-fonts', () => {
-	return gulp
-		.src('src/assets/fonts/**')
-		.pipe(plumber({ errorHandler: onError }))
-		.pipe(gulp.dest('app/assets/fonts'));
-});
-
-gulp.task('process-images', () => {
-	return gulp
-		.src('src/assets/img/**')
+function processImages() {
+	return src(['./src/assets/img/**', '!./src/assets/img/**/*.ico'])
 		.pipe(plumber({ errorHandler: onError }))
 		.pipe(
 			imagemin([imagemin.svgo({ plugins: [{ removeViewBox: true }] })], {
 				verbose: true,
 			}),
 		)
-		.pipe(gulp.dest('app/assets/img'));
-});
-//--------------------------------------------------------------------------------------------------
-/* -------------------------------------------------------------------------------------------------
-Utilitie Tasks
--------------------------------------------------------------------------------------------------- */
-const swb = '\x1b[44m\x1b[1mStatic Web Build\x1b[0m';
-const swbUrl = '\x1b[2m - https://staticbuild.website/\x1b[0m';
-const thankYou = 'Thank you for using the ' + swb + swbUrl;
-const errorMsg = '\x1b[41mError\x1b[0m';
-const filesGenerated =
-	'Your distribution files are generated in: \x1b[1m' + __dirname + '/app/' + '\x1b[0m - ✅';
+		.pipe(dest('./dist/assets/img'))
+		.on('end', () => {
+			gutil.beep();
+			gutil.log(filesGenerated);
+			gutil.log(thankYou);
+		});
+}
 
-const onError = function(err) {
+exports.prod = series(
+	cleanProd,
+	copyFontsProd,
+	headerScriptsProd,
+	footerScriptsProd,
+	staticFilesProd,
+	processImages,
+);
+
+/* -------------------------------------------------------------------------------------------------
+Utility Tasks
+-------------------------------------------------------------------------------------------------- */
+const onError = err => {
 	gutil.beep();
-	console.log(swb + ' - ' + errorMsg + ' ' + err.toString());
+	gutil.log(staticBuild + ' - ' + errorMsg + ' ' + err.toString());
 	this.emit('end');
 };
+
+/* -------------------------------------------------------------------------------------------------
+Messages
+-------------------------------------------------------------------------------------------------- */
+const errorMsg = '\x1b[41mError\x1b[0m';
+const filesGenerated =
+	'Your production file are generated in: \x1b[1m' +
+	__dirname +
+	'/dist/ ✅';
+
+const staticBuild = '\x1b[42m\x1b[1mStaticWebBuild\x1b[0m';
+const staticBuildUrl = '\x1b[2m - https://staticbuild.website/\x1b[0m';
+const thankYou = 'Thank you for using ' + staticBuild + staticBuildUrl;
+
 /* -------------------------------------------------------------------------------------------------
 End of all Tasks
 -------------------------------------------------------------------------------------------------- */
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, './app')));
 
 app.get('/*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../app', 'index.html'));
+	res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
 const port = process.env.PORT || 9000;
