ae61879bdf6861b10c3f1a85c435d492b4860ac8
Luan Gjokaj <luan@riangle.com>
2019-04-24 00:21:57 +0200

    🐛 FIX: Server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, './app')));
+app.use(express.static(path.join(__dirname, './dist')));
 
 app.get('/*', (req, res) => {
 	res.sendFile(path.join(__dirname, '../dist', 'index.html'));
